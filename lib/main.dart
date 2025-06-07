6f0a9cef9d5343d47c26cfecfbe804b19b45b111
Charithm <charithm@codegen.net>
2025-06-07 23:15:26 +0530

    Add wakelock_plus package to dependencies and implement screen wake lock in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 import 'app/theme/app_theme.dart';
 import 'features/webview/presentation/pages/webview_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  
+  // Enable wakelock to keep screen on
+  await WakelockPlus.enable();
+  
   runApp(const MyApp());
 }
 
